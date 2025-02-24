abe11d172c8867fec33d0a1f7c0cf9888da51c9c
Abrar Ahmed <comsianabrar@gmail.com>
2025-02-24 05:55:37 +0500

    fix

--- src/components/FormFooter.jsx
+++ src/components/FormFooter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from './ui/Button'
+import { Button } from '../components/ui/button'
 import { useDispatch, useSelector } from 'react-redux'
 import { handleBackButton } from '../store/stepsSlice'
 
